de2810e43d93356ceaf797ebc3372329ef0dd410
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-09-20 17:44:58 +0530

    footer
    Co-Authored-By: Nethmi Rashmika <103495333+Nethmi31@users.noreply.github.com>

---
+++ HTML/footer.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/main.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+
+    <title>index</title>
+  </head>
+  <body>
+  
+                  
+    
+                  
+<footer  style="background-color:#333;"class="text-center text-lg-start  text-muted p-4">
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h3 class="text-uppercase fw-bold mb-4 text-info">
+            <i class="fas fa-gem me-3 "></i>Zara.lk
+          </h3>
+          <img class="col-lg-6" src="../PHOTO/logooo.png" alt="logo">
+          <!--description ekk dpm-->
+         
+        </div>
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h5 class="text-uppercase fw-bold mb-4 text-info ">
+            Need a Help ?
+          </h5>
+          <p>
+            <a style="text-decoration:none ;;"href="#!" class="text-reset">Terms</a>
+          </p>
+          <p>
+            <a style="text-decoration:none ;" href="#!" class="text-reset">Conditions</a>
+          </p>
+          <p>
+            <a style="text-decoration:none ;"href="#!" class="text-reset">Help</a>
+          </p>
+          <p>
+            <a style="text-decoration:none ;"href="#!" class="text-reset">Setting</a>
+          </p>
+        </div>
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h5 class="text-uppercase fw-bold mb-4 text-info">
+            Contact US..
+          </h5>
+          <p>
+           <a style="text-decoration:none ;"href="#" class="text-reset">Zara logistic</a>
+          </p>
+          <p>
+            <a style="text-decoration:none ;"href="#!" class="text-reset">Tel:0469800565</a>
+          </p>
+          <p>
+            <a style="text-decoration:none ;"href="#!" class="text-reset">Galle Road Matara</a>
+          </p>
+          <p>
+            <a style="text-decoration:none ;"href="#!" class="text-reset">zara@hotmail.com</a>
+          </p>
+        </div>
+        <div class="col-md-3">
+              <h5 class="text-uppercase fw-bold mb-4 text-info">Follow Us</h5>
+                <a class="btn btn-primary" style="background-color:#06032e;" href="#!" role="button"
+  ><i class="fa fa-facebook"></i
+></a>
+                <a class="btn btn-primary" style="background-color:#06032e;" href="#!" role="button"
+                   ><i class="fa fa-instagram"></i></a>
+                <a class="btn btn-primary" style="background-color:#06032e;" href="#!" role="button"
+                   ><i class="fa fa-twitter"></i></a>
+                <a class="btn btn-primary" style="background-color:#06032e;" href="#!" role="button"
+                   ><i class="fa fa-linkedin"></i></a>
+           
+            </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="https://Zara/lk/">Zara.lk</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+
+
+
+      
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script> 
+
+  </body>
+</html>
