e44f2ee8d36752713e18964f8f32b21b83eae210
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-09-20 17:16:29 +0530

    about

---
+++ HTML/about.php
@@ -0,0 +1,34 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.about-section-about{
+  padding: 180px;
+  text-align: center;
+  background-color: #474e5d;
+  color: white;
+}
+</style>
+</head>
+<body">
+  <?php
+  require_once("header.php");
+  ?>
+
+<div class="about-section-about">
+  <h1>About Us Page</h1>
+  <p>Founded here in the Paradise Island of Sri Lanka in Year 2017, ZARA is an inclusive and innovative brand targeting value oriented customers. It has been pushing boundaries to bring its customers up-to-date and inspirational fashion.</p>
+  <p>Zara has grown rapidly in bringing the versatile Modern Women of today with magnificent taste in Fashion. We are a family of fashion enthusiasts, driven by our desire to make exceptional designs maintaining sustainability while aiming to be the leading e-Commerce and offline Women’s Fashion brand in Sri Lanka. .</p>
+</div>
+
+
+<?php
+  require_once("footer.php");
+  ?>
+
+</body>
+</html>
