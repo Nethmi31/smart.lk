7ddab2d62dbe1ae8d1da296b4bc076dd4b98152b
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-09-21 22:23:54 +0530

    up

---
+++ HTML/header.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+   <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Bootstrap CSS -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <link rel="stylesheet" href="../CSS/main.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   </head>
+   <body>
+      <!--TOP hedaer-->
+      <div class="container-fixed desc1-header fixed-top">
+         <div class="top-header">
+            <div class="top-title">
+               <p>ZARA.LK</p>
+            </div>
+         </div>
+         <div class="col-sm-12 nav-top" id="header_second">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+               <div class="container-fluid">
+                  <a class="navbar-brand" href="#">Menu</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                           <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="about.php">About</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="mini_dress.php">Mini</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="new_maxi_dress.php">Maxi</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                           category
+                           </a>
+                           <ul class="dropdown-menu" aria-labelledby="navbarDropdown" >
+                              <li><a class="dropdown-item" href="new_maxi_dress.php">Maxi</a></li>
+                              <li><a class="dropdown-item" href="mini_dress.php">Mini</a></li>
+                              <li>
+                                 <hr class="dropdown-divider">
+                              </li>
+                           </ul>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link disabled"></a>
+                        </li>
+                     </ul>
+                     <div class="topnav-right" style="float: right;">
+                        <a style="padding-right:10px;"href="login_&_registration.php"><i class="fa fa-user fa-2x"></i></a>
+                        <a  style="padding-right:10px;"href="cart.php"><i class="fa fa-shopping-cart fa-2x"></i></a>
+                        <a  style="padding-right:10px;"href="logout.php"><i class="fa fa-sign-out fa-2x"></i></a>
+                     </div>
+                  </div>
+               </div>
+            </nav>
+         </div>
+      </div>
+      </div>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+      <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script> 
+   </body>
+</html>
